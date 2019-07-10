06d679b3ff8fdc0103a3118d1a7e2412eaa5c80b
Francisco Abarzúa <git.id@frabarz.cl>
2019-07-10 18:47:50 -0400

    fixes bug on pointer to restui volume

--- rebuild_restui.sh
+++ rebuild_restui.sh
@@ -5,6 +5,6 @@ DOMAIN=$1
 sudo docker build -t restui:latest https://github.com/Datawheel/mondrian-rest-ui.git#master
 # - run the image in a temporary container and save the build in a volume
 #  (the datachile_hddrestui volume will be handled later by docker-compose, update it if needed)
-sudo docker run --rm --volume datachile_hddrestui:/app/src/build -e "REACT_APP_API_URL=https://chilecube.$DOMAIN" restui
+sudo docker run --rm --volume ${PWD##*/}_hddrestui:/app/src/build -e "REACT_APP_API_URL=https://chilecube.$DOMAIN" restui
 # - delete the docker image
 sudo docker rmi restui
